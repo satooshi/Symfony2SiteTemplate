179e394033893dd0d9d4c505e1c168c0bcb3f397
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-01-14 21:37:48 +0900

    fix bug: fail to change dir

--- bin/refresh-vendor.sh
+++ bin/refresh-vendor.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-bin_dir=$(cd $(dirname $0);pwd)
-root_dir=$(cd ../;pwd)
+bin_dir=`dirname $0`
+root_dir=`cd ${bin_dir}; cd ../;pwd`
 
-cd ${root_dir}
+cd $root_dir
 
 sudo rm -rf app/cache/* composer.lock vendor
 composer install > composer.log
 sudo rm -rf app/cache/*
-rm -f vendor.tar.gz 
+rm -f vendor.tar.gz
 tar zcvf vendor.tar.gz vendor  >/dev/null 2>&1
